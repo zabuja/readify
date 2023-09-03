e7ca19f541259582e2e65064342af2e8b50a2e48
atanu <atanu.tmsl@gmail.com>
2023-09-03 14:47:04 +0530

    feat: signup/sigin page added

--- src/pages/UserAuthPage.jsx
+++ src/pages/UserAuthPage.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
+import React from "react";
+import girlSVG from "../images/girlSVG.png";
+import Facebook from "../svgs/Facebook";
+import Twitter from "../svgs/Twitter";
+import Google from "../svgs/Google";
 
 const UserAuthPage = () => {
   return (
-    <div>
-        <div className='left-side'>
-            Welcome Back!
-            <br/>
-            Unlocking a World of Possibilities with Readify App. Get started for free.
-
-            <form>
-               <input type="text"></input>
-               <input type="text"></input>
-               <input type="submit" value="submit"></input>
-            </form>
+    <div className="container">
+      <div className="left-side">
+        <h1>Welcome Back!</h1>
+        <br />
+        <h2>
+          Unlocking a World of Possibilities with Readify App. Get started for
+          free.
+        </h2>
+        <form className="form-container">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Username"
+          ></input>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          ></input>
+          <br />
+          <div className="forgot-pwd">Forgot Password?</div>
+          <input type="submit" value="submit"></input>
+        </form>
+        <div className="design-container">
+          <hr/>
+          <div className="text">or continue with</div>
+          <hr/>
         </div>
-
-        <div className='right-side'>
-           <img src="src\images\Girl-Workplace.png"></img>
+        <div className="icons">
+          <Facebook />
+          <Google />
+          <Twitter />
         </div>
+        
+        <div className="end-text">Not a member?<a href="#"> Register now</a></div>
+      </div>
+
+      <div className="right-side">
+        <img src={girlSVG} alt="girl illustration" className="image-girl" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserAuthPage
+export default UserAuthPage;
