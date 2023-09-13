726a8c6c0f5b549702466744e3e770c368521837
atanu <atanu.tmsl@gmail.com>
2023-09-13 06:22:21 +0530

    feat: drag and drop added

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,40 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["EPUB", "PDF"];
 
 const Hero = () => {
-  return (
-    <section className="mt-16 w-full max-w-xl">
-      <div className="flex flex-col w-full gap-2">
-        <form
-          className="relative flex justify-center items-center"
-          onSubmit={handleSubmit}
-        >
-          <img
-            src={linkIcon}
-            alt="link icon"
-            className="absolute left-0 my-2 ml-3 w-5"
-          />
-          <input
-            type="url"
-            placeholder="Enter your URL"
-            value={article.url}
-            onChange={(e) => setarticle({...article, url: e.target.value})}
-            required
-            className="url_input peer"
-          ></input>
-          <button
-            type="submit"
-            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
-          >
-            ⏎
-          </button>
-        </form>
 
-        {/* Browse URL history */}
-      </div>
-      {/* Display results */}
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  return (
+    <section className='m-16 w-full max-w-md'>
+      <div className='p-5 ' >
+      <h1 className='pb-5 text-lg font-bold'>Drag & Drop Files</h1>
+      <FileUploader
+        multiple={true}
+        handleChange={handleChange}
+        name="file"
+        types={fileTypes}
+      />
+      <p className='pt-5 text-red-500'>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
+    </div>
     </section>
   )
 }
 
-export default Hero
+export default Hero;
+
